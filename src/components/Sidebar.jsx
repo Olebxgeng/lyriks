ff79f48293690de83ab5ac1b535944eee1b4d7b4
Olebogeng Sebogodi <sebogodio@icloud.com>
2024-01-16 22:30:44 +0200

    Top Play, Top Charts, Song Details

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,12 +33,12 @@ const Sidebar = () => {
       </div>
       <div className="absolute md:hidden block top-6 right-3">
         { mobileMenuOpen ? (
-          <MdClose className="w-6 h-6 text-white mr-2" />
-        ) : <MdMenu className="w-6 h-6 text-white mr-2" /> }
+          <MdClose className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />
+        ) : <MdMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} /> }
       </div>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-        <NavLinks />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,17 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': 'ad4d3c1f3bmsh4338edeaf6100ebp126866jsn0df096e3a006',
-//     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
-//   },
-// };
-
-// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
 
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
@@ -25,7 +13,10 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
   }),
+
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery } = shazamCoreApi;
